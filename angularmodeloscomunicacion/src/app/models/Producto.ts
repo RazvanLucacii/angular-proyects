aa57f26865a5ddda5adae814926f60c77016ac34
RazvanLucacii <90837528+RazvanLucacii@users.noreply.github.com>
2023-11-14 10:44:21 +0100

    Delete angularmodeloscomunicacion directory

--- angularmodeloscomunicacion/src/app/models/Producto.ts
+++
@@ -1,9 +0,0 @@
-export class Producto{
-    //tenemos un constructor para recibir los datos de un producto
-    constructor(
-        public nombre: string,
-        public imagen: string,
-        public precio: number,
-    ){}
-
-}
